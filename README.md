b4272a47546416e17bc047859603c069f496a14c
Chris Atkin <atkinchris@gmail.com>
2020-05-02 09:49:35 +0100

    Add README, run.sh and docker-compose from Ara CLI

---
+++ README.md
@@ -0,0 +1,7 @@
+# Micro Frontends Playground
+
+This is a repository for me to test micro frontend architecture, and it's related ecosystem.
+
+## Running
+
+Clone this repository, change to the cloned directory, then execute `./run.sh`.
---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Fail on the first error, rather than continuing
+set -e
+
+function on_exit {
+  docker-compose stop
+  docker-compose kill
+  docker-compose rm -vf
+}
+
+trap on_exit EXIT
+
+docker-compose kill
+docker-compose rm -vf
+docker-compose build
+docker-compose up --remove-orphans --abort-on-container-exit $@
