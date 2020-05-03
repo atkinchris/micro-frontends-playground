795e4ced91eaea9f1c012757e5f648fa29b5a855
Chris Atkin <atkinchris@gmail.com>
2020-05-03 11:39:23 +0100

    Replace upstream with basic Express server

---
+++ services/upstream/index.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+
+const port = process.env.PORT || 8080
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Example Website' })
+})
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+)
