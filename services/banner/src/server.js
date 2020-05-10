19a9af7b42894d63dc90699fa32e94520a70bd7a
Chris Atkin <atkinchris@gmail.com>
2020-05-10 19:49:23 +0100

    Strip away surrounding HTML to leave fragment

--- services/banner/src/server.js
+++ services/banner/src/server.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { renderToString } from 'react-dom/server'
 
 import App from './components/App'
+import constants from './constants'
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST)
 
@@ -11,33 +12,18 @@ server
   .disable('x-powered-by')
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
   .get('/*', (req, res) => {
-    const context = {}
     const markup = renderToString(<App />)
 
-    if (context.url) {
-      res.redirect(context.url)
-    } else {
-      res.status(200).send(
-        `<!doctype html>
-    <html lang="">
-    <head>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta charset="utf-8" />
-        <title>Welcome to Razzle</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    res.status(200).send(
+      `
         ${assets.client.css ? `<link rel="stylesheet" href="${assets.client.css}">` : ''}
         ${
           process.env.NODE_ENV === 'production'
             ? `<script src="${assets.client.js}" defer></script>`
             : `<script src="${assets.client.js}" defer crossorigin></script>`
         }
-    </head>
-    <body>
-        <div id="root">${markup}</div>
-    </body>
-</html>`
-      )
-    }
+        <div id="${constants.ROOT_ID}">${markup}</div>`
+    )
   })
 
 export default server
