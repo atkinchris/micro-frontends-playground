595c585aa18415df7f53015212ca5c8de516b07e
Chris Atkin <atkinchris@gmail.com>
2020-05-10 19:35:17 +0100

    Replace component with basic Razzle setup

---
+++ services/banner/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+import Home from './Home'
+import './App.css'
+
+const App = () => <Home />
+
+export default App
--- services/component/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  rootDir: 'src',
-  resetMocks: true,
-  resetModules: false,
-  setupFiles: ['<rootDir>/setupTestFramework.ts'],
-}
